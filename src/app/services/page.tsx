5266213d4f6cd613d3450d79d0a49554015b4c90
ivan <ivan@loyalto.id>
2024-07-09 17:34:06 +0700

    fix: og image issue

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     siteName: "Tembusin",
     images: [
       {
-        url: "/assets/og-image.png",
+        url: "https://tembusin.id/assets/og-image.png",
         width: 1200,
         height: 630,
       },
